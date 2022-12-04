81229dc71054fce9374dcd27ea4ffe725dd72167
Mino <dev@mino.one>
2022-12-04 17:10:28 -0300

    weeeeeeeee

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "osu-phantom/libgo"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
-	libgo.GetPP()
+	var file, _ = ioutil.ReadFile("test.osu")
+	var req, _ = http.NewRequest("POST", fmt.Sprintf("http://localhost:8080/diff?mods=%d", 1<<4), bytes.NewBuffer(file))
+	req.Header.Set("Content-Type", "text/osu")
+	var res, _ = http.DefaultClient.Do(req)
+	io.Copy(os.Stdout, res.Body)
+
+	//libgo.GetPP()
 	//Client := &client.PhantomClient{
 	//	Username: "minoxs",
 	//}
