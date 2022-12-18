1108a950e3b1d706969388d491cfee17f014018d
Mino <dev@mino.one>
2022-12-17 21:59:38 -0300

    Add crosu as dependency

--- libgo/crosu.go
+++ libgo/crosu.go
@@ -1,12 +1,12 @@
 package libgo
 
 /*
-	This package handles the switch between Go and C code
+	This package handles the bridge between Go and C code
 */
 
 /*
-#cgo CFLAGS: -I ../inc
-#cgo LDFLAGS: -L ../libc -lcrosu_pp
+#cgo CFLAGS: -I ../deps/crosu-pp/bindings
+#cgo LDFLAGS: -L ../deps/crosu-pp/target/release -lcrosu_pp
 #include "crosu.h"
 */
 import "C"
@@ -15,6 +15,30 @@ import (
 	"io/ioutil"
 	"unsafe"
 )
+
+type ModType C.u32
+type Usize C.size_t
+
+const (
+	NF ModType = 1 << 0
+	EZ         = 1 << 1
+	TD         = 1 << 2
+	HD         = 1 << 3
+	HR         = 1 << 3
+	DT         = 1 << 4
+	RX         = 1 << 5
+	HT         = 1 << 6
+	FL         = 1 << 7
+	SO         = 1 << 8
+)
+
+type Score struct {
+	Combo  Usize
+	N300   Usize
+	N100   Usize
+	N050   Usize
+	Misses Usize
+}
 
 func GetPP() {
 	buf, _ := ioutil.ReadFile("./test.osu")
